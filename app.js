739adaf6bc2e15c1f634a74f27138ec117f3d170
mzubairdev <mzubair5978@gmail.com>
2024-12-13 12:13:57 +0500

    chapter-02 completed

--- app.js
+++ app.js
@@ -25,8 +25,53 @@
 
 // 6. Make use of alerts in your new/existing HTML & CSS project.
 
-// 7. Practice placement of <script></script> element in following sections of your project in exercise 
+// 7. Practice placement of <script></script> element in following sections of your project in exercise
 // 6: a. Head b. Body (before your page's HTML) c. Body (Inside your page's HTML)  d. Body (After your page's HTML).
 // Chapter-01 ended.
 
 // chapter-02 started:
+// 1. Declare a variable called username.
+// var username;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName = "Muhammad Zubair";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign "Hello World" to variable message.
+// c) Display the message in alert box.
+// var message = "Hello World";
+// alert(message);
+
+// 4. Write a script to save student's bio data in JS variables and show the data in alert.
+
+// var studentName = "Johne Doe";
+// var age = 15;
+// var course = "Certified Mobile Application development";
+// alert(studentName);
+// alert(age + " " + "years old");
+// alert(course);
+
+// 5. Write a script to display the following alert using one JS variable.
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box. (Hint: string concatenation).
+
+// var email = "mzubair@gmail.com";
+// alert("My email address is " + email);
+
+// 7. Declare a variable called book & give it the value "A smarter way to learn JavaScript". Display the following message in an alert box.
+
+// var book = "I am trying to learn from the Book A smarter\nway to learn JavaScript";
+// alert(book);
+
+// 8. Write a script to display this in browser through JS
+// var onBrowser = "Yah! I can write HTML content thorught JavaScript";
+// document.write(onBrowser);
+
+// 9. Store following string in a variable and show in alert and browser through JS
+// var specialDesign = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(specialDesign);
+// Chapter-02 ended
