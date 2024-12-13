62020dfba1860411e6d05a5460dba4dc64732781
mzubairdev <mzubair5978@gmail.com>
2024-12-13 11:43:11 +0500

    chaper-01 completed

--- app.js
+++ app.js
@@ -4,4 +4,27 @@
 // 1. Write a script to greet your website visitor using JS alert box.
 // alert("Hello Welcome To Javascript World!");
 
+// 2. Write a script to display following message on your web page.
+// alert("Error! Please enter a valid password!");
 
+// 3. Write a script to display following message on your web page: (Hint: Use line break).
+// alert(" Welcome to JS Land... \n Happy Coding! ");
+
+// 4. Write a script to display following message in sequence.
+// alert("Welcome to JS Land...");
+// alert("Happy Coding");
+
+// 5. Generate the following message through browser's developer console.
+
+// Instructions:
+// 1. Open your browser's developer tools.
+//    - Press Ctrl + Shift + J (Windows).
+// 2. Copy the following code.
+// 3. Paste it into the developer console and press Enter to see the output.
+// console.log("Hello... I can run JS through my web browser's console.");
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+
+// 7. Practice placement of <script></script> element in following sections of your project in exercise 
+// 6: a. Head b. Body (before your page's HTML) c. Body (Inside your page's HTML)  d. Body (After your page's HTML).
+// Chapter-01 ended.
