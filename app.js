d630f55c7d0c3109cf5a679567019b17781b73fa
mzubairdev <mzubair5978@gmail.com>
2024-12-19 16:21:10 +0500

    js-assignment update

--- app.js
+++ app.js
@@ -81,7 +81,7 @@
 // var age = 20;
 // alert("I am " + age + " years old");
 
-// 2. Declare & initialize a variable to keepdocument.write track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: "You have visited this site N times".
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: "You have visited this site N times".
 // var visitCount = 14;
 // console.log("You have visited this site " + visitCount + " times");
 
