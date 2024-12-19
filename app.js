4de7c1f678f80f6c162f302b12112b1d1cd731d0
mzubairdev <mzubair5978@gmail.com>
2024-12-19 16:10:05 +0500

    js-assignment update

--- app.js
+++ app.js
@@ -81,24 +81,24 @@
 // var age = 20;
 // alert("I am " + age + " years old");
 
-// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: "You have visited this site N times".
+// 2. Declare & initialize a variable to keepdocument.write track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: "You have visited this site N times".
 // var visitCount = 14;
-// document.write("You have visited this site " + visitCount + " times");
+// console.log("You have visited this site " + visitCount + " times");
 
 // 3. Declare a variable called birth Year & assign to it your birth year. Show the following message in your browser:
 // var birthYear = 2005;
-// document.write("My birth year is " + birthYear +"<br>Data Type of my declared variable is " + typeof(birthYear));
+// console.log("My birth year is " + birthYear +" Data Type of my declared variable is " + typeof(birthYear));
 
 // 4. A visitor visits an online clothing store www.xyzClothing.com. Write a script to store in variables the following information:
-// a. Visitor's name 
-// b. Product title 
+// a. Visitor's name
+// b. Product title
 // c. Quantity i.e. how many products a visitor wants to order
 // Show the following message in your browser: "John Doe ordered 5 T-shirt(s) on XYZ Clothing store".
 // var visitorName = "Johne Doe";
 // var productTitle = "T-shirt";
 // var size = "(s)";
 // var quantity = 5;
-// document.write("<b>" + visitorName + "</b>" + " ordered " + "<b>" + quantity + " " + productTitle + "</b>" + size + " on XYZ Clothing Store ");
+// console.log(visitorName + " ordered " + quantity + " " + productTitle + size + " on XYZ Clothing Store");
 // Chapter-03 ended
 
 // Chapter-04 started:
@@ -120,16 +120,16 @@
 // var break;
 // var variable-name1;
 
-// 3. Display this in your browser 
-// // a) A heading stating "Rules for naming JS variables"
-// // b) Variable names can only contain _________,_________and_________. For example $my_1stVariable
-// document.write("Variable names can only contain numbers, $ and _. For example: $my_1stVariable <br>");
-// // c) Variables must begin with a_________,_________or_________. For example $name, _name or name 
-// document.write("Variables must begin with a letter, $ and _ . For example $name, _name or name <br>");
+// 3. Display this in your browser
+// a) A heading stating "Rules for naming JS variables"
+// b) Variable names can only contain _________,_________and_________. For example $my_1stVariable
+// console.log("Variable names can only contain numbers, $ and _. For example: $my_1stVariable");
+// // c) Variables must begin with a_________,_________or_________. For example $name, _name or name
+// console.log("Variables must begin with a letter, $ and _ . For example $name, _name or name");
 // // d) Variable names are case _________.
-// document.write("Variable names are case sensitive <br>");
+// console.log("Variable names are case sensitive");
 // // e) Variable names should not be JS _________.
-// document.write("Variable names should not be JS keywords");
+// console.log("Variable names should not be JS keywords");
 // Chapter-04 ended
 
 // Chapter-05 started:
@@ -138,27 +138,26 @@
 // var num1 = 3;
 // var num2 = 5;
 // var addition = num1 + num2 ;
-// document.write("Sum of " + num1 + " and " + num2 + " is " + addition + "<br>");
+// console.log("Sum of " + num1 + " and " + num2 + " is " + addition);
 
 // // 2. Repeat task1 for subtraction, multiplication, division & modulus.
 // //// subtraction
 // var subtraction = num1 - num2 ;
-// document.write(" Subtraction of " + num1 + " and " + num2 + " is " + subtraction + "<br>");
+// console.log(" Subtraction of " + num1 + " and " + num2 + " is " + subtraction);
 // //// multiplication
 // var multiplication = num1 * num2 ;
-// document.write("Multiplication of " + num1 + " and " + num2 + " is " + multiplication + "<br>");
+// console.log("Multiplication of " + num1 + " and " + num2 + " is " + multiplication);
 // //// division
 // var division = num1 / num2 ;
-// document.write("Division of " + num1 + " and " + num2 + " is " + division + "<br>");
+// console.log("Division of " + num1 + " and " + num2 + " is " + division);
 // //// modulus
 // var modulus = num1 % num2 ;
-// document.write("Modulus of " + num1 + " and " + num2 + " is " + modulus);
-
+// console.log("Modulus of " + num1 + " and " + num2 + " is " + modulus);
 
 // 3. Do the following using JS Mathematic Expressions
-// a. Declare a variable. 
-// b. Show the value of variable in your browser like "Value after variable declaration is: ??". 
-// c. Initialize the variable with some number. 
+// a. Declare a variable.
+// b. Show the value of variable in your browser like "Value after variable declaration is: ??".
+// c. Initialize the variable with some number.
 // d. Show the value of variable in your browser like "Initial value: 5".
 // e. Increment the variable.
 // f. Show the value of variable in your browser like "Value after increment is: 6".
@@ -169,58 +168,58 @@
 // k. Show the remainder after dividing the variable's value by 3.
 // l. Output: "The remainder is: 0".
 // var undefined;
-// document.write("Value after variable declaration is " + undefined + "<br>");
+// console.log("Value after variable declaration is " + undefined);
 // var num = 5;
-// document.write("Initial Value " + num + "<br>");
+// console.log("Initial Value " + num);
 // num += 1;
-// document.write("Value after increment is: " + num + "<br>");
+// console.log("Value after increment is: " + num);
 // num += 7;
-// document.write("Value after adition is: " + num + "<br>");
+// console.log("Value after adition is: " + num);
 // num -= 1;
-// document.write("Value after decrement is: " + num + "<br>");
+// console.log("Value after decrement is: " + num);
 // num %= 3;
-// document.write("The remainder is: " + num + "<br>");
+// console.log("The remainder is: " + num);
 
 // 4. Cost of one movie ticket is 600 PKR.
 // Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie.
 // Example output.
 // var cost = 600;
 // var totalCost = cost * 5;
-// document.write("Total cost to buy 5 tickets to a movie is " + totalCost + "PKR");
+// console.log("Total cost to buy 5 tickets to a movie is " + totalCost + "PKR");
 
 // 5. Write a script to display multiplication table of any number in your browser. E.g.
 // var tableNumber = 4;
 // var num = 1;
-// document.write("Table of " + tableNumber + " <br />");
+// console.log("Table of " + tableNumber);
 // var result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 // ++num;
 // result = tableNumber * num;
-// document.write( tableNumber + " x " + num + " = " + result , "<br />");
+// console.log( tableNumber + " x " + num + " = " + result);
 
 // 6. The Temperature Converter: It's hot out! Let's make a converter based on the steps here.
 // a. Store a Celsius temperature into a variable.
@@ -229,10 +228,10 @@
 // d. Convert it to Celsius & output "NNoF is NNOC".
 // var celsiusTemperature = 25;
 // var conversion1 = (celsiusTemperature * 9/5)+32;
-// document.write(celsiusTemperature + "°C is " + conversion1 + "°F <br>");
+// console.log(celsiusTemperature + "°C is " + conversion1 + "°F");
 // var fahrenheitTemp = 70;
 // var conversion2 = (fahrenheitTemp - 32)*5/9;
-// document.write(fahrenheitTemp + "°F is " + conversion2 + "°C");
+// console.log(fahrenheitTemp + "°F is " + conversion2 + "°C");
 
 // 7. Write a program to implement checkout process of a shopping cart system for an e-commerce website. Store the following in variables.
 // a. Price of item 1
@@ -247,48 +246,48 @@
 // var quantityItem2 = 7;
 // var shipcharges = 100;
 // var totalCost = ($Item1 * 3) + ($Item2 * 7) + shipcharges;
-// document.write("Price of item 1 is " + $Item1 , "<br>");
-// document.write("Quantity of item 1 is " + quantityItem1 , "<br>");
-// document.write("Price of item 2 is " + $Item2 , "<br>");
-// document.write("Quantity of item 2 is " + quantityItem2 , "<br>");
-// document.write("Shipping Charges " + shipcharges , "<br>");
-// document.write("Total cost  of your order is " + totalCost , "<br>");
+// console.log("Price of item 1 is " + $Item1);
+// console.log("Quantity of item 1 is " + quantityItem1);
+// console.log("Price of item 2 is " + $Item2);
+// console.log("Quantity of item 2 is " + quantityItem2);
+// console.log("Shipping Charges " + shipcharges);
+// console.log("Total cost  of your order is " + totalCost);
 
 // 8. Store total marks & marks obtained by a student in 2 variables. Compute the percentage & show the result in your browser.
 // var totalMarks = 980;
 // var marksObtained = 804;
 // var percentage = marksObtained / totalMarks * 100;
-// document.write("Total marks " + totalMarks + "<br>");
-// document.write("Marks obtained " + marksObtained + "<br>");
-// document.write("Percentage " + percentage + "%");
+// console.log("Total marks " + totalMarks);
+// console.log("Marks obtained " + marksObtained);
+// console.log("Percentage " + percentage + "%");
 
 // 9. Assume we have 10 US dollars & 25 Saudi Riyals.
 // Write a script to convert the total currency to Pakistani Rupees.
 // Perform all calculations in a single expression.
 // (Exchange rates: 1 US Dollar = 104.80 Pakistani Rupee and 1 Saudi Riyal = 28 Pakistani Rupee).
 // var exchangeRate =  ((104.80 * 10) + (28*25));
-// document.write(" Total Currency in PKR: " + exchangeRate);
+// console.log(" Total Currency in PKR: " + exchangeRate);
 
-// 10. Write a program to initialize a variable with some number and do arithmetic in following sequence: 
-// a. Add 5 
-// b. Multiply by 10 
+// 10. Write a program to initialize a variable with some number and do arithmetic in following sequence:
+// a. Add 5
+// b. Multiply by 10
 // c. Divide the result by 2
 // Perform all calculations in a single expression
 // var num = 10;
 // var calc = ((num+5)*10)/2;
-// document.write(calc);
+// console.log(calc);
 
-// 11. The Age Calculator: Forgot how old Calculate it! someone is? 
-// a. Store the current year in a variable. 
-// b. Store their birth year in a variable. 
+// 11. The Age Calculator: Forgot how old Calculate it! someone is?
+// a. Store the current year in a variable.
+// b. Store their birth year in a variable.
 // c. Calculate their 2 possible ages based on the stored values.
 // var currentYear = 2024;
 // var dob = 2005;
 // var age = currentYear-dob;
 // var age2 = age - 1;
-// document.write("Your age could be either " + age + " or " + age2);
+// console.log("Your age could be either " + age + " or " + age2);
 
-// 12. The Geometrizer: Calculate properties of a circle. 
+// 12. The Geometrizer: Calculate properties of a circle.
 // a. Store a radius into a variable.
 // b. Calculate the circumference based on the radius, and output "The circumference is NN".
 // (Hint: Circumference of a circle = 2 πr, π = 3.142)
@@ -297,15 +296,15 @@
 // var radius = 20;
 // var circumference = ((2 * 3.142) * radius);
 // var area = (3.142 *(radius * radius));
-// document.write("Radius of a circle: " + radius + "<br>");
-// document.write("The circumference is: " + circumference + "<br>");
-// document.write("The area is: " + area);
+// console.log("Radius of a circle: " + radius);
+// console.log("The circumference is: " + circumference);
+// console.log("The area is: " + area);
 
-// 13. The Lifetime Supply Calculator: Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more. 
-// a. Store your favorite snack into a variable 
-// b. Store your current age into a variable. 
-// c. Store a maximum age into a variable. 
-// d. Store an estimated amount per day (as a number). 
+// 13. The Lifetime Supply Calculator: Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more.
+// a. Store your favorite snack into a variable
+// b. Store your current age into a variable.
+// c. Store a maximum age into a variable.
+// d. Store an estimated amount per day (as a number).
 // e. Calculate how many would you eat total for the rest of your life.
 // Output the result to the screen like so: "You will need NNNN to last you until the ripe old age of NN".
 // var favSnack = "chocolate chip";
@@ -313,9 +312,9 @@
 // var maxAge = 65;
 // var amountPerDay = 3;
 // var totalEat = ((maxAge - currentAge) * 365) * amountPerDay;
-// document.write("Favourite Snack: " + favSnack + "<br>");
-// document.write("Current age: " + currentAge + "<br>");
-// document.write("Estimated Maximum Age " + maxAge + "<br>");
-// document.write("Amount of snaks per day " + amountPerDay + "<br>");
-// document.write("You will need " + totalEat + " chocolate chip to last you until the ripe old age of " + maxAge);
+// console.log("Favourite Snack: " + favSnack);
+// console.log("Current age: " + currentAge);
+// console.log("Estimated Maximum Age " + maxAge);
+// console.log("Amount of snaks per day " + amountPerDay);
+// console.log("You will need " + totalEat + " chocolate chip to last you until the ripe old age of " + maxAge);
 // Chapter-05 ended
